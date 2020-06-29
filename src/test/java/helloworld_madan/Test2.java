6189450f132c53e31facf15c035acbd38a1c13a6
Madan Kumar <madansreeram@gmail.com>
2020-06-29 07:47:18 +0000

    My First Maven Commit

---
+++ src/test/java/helloworld_madan/Test2.java
@@ -0,0 +1,35 @@
+package helloworld_madan;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+		int a = 30;
+		int b = 20;
+		Assert.assertEquals(50, a+b);
+	}
+	@Test
+	public void sub() {
+		System.out.println("SUB");
+		int a = 30;
+		int b = 20;
+		Assert.assertEquals(10, a-b);
+	}
+	@Test
+	public void div() {
+		System.out.println("DIV");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b/a);
+	}
+	@Test
+	public void mul() {
+		System.out.println("MUL");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a*b);
+	}
+}
